bfe83e20923cfb81db0308963dafb5790c4c7358
Junfan Zhang <zuston@apache.org>
2025-02-25 10:26:36 +0800

    feat: Support sorting for the apps http dashboard

--- src/http/apps.rs
+++ src/http/apps.rs
@@ -42,16 +42,74 @@ fn table() -> Html<String> {
                 background-color: #f5f5f5;
             }
         </style>
+
+        // refer: https://www.w3schools.com/howto/howto_js_sort_table.asp
+        <script>
+            function sortTable(n) {
+              var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+              table = document.getElementById("myTable2");
+              switching = true;
+              // Set the sorting direction to ascending:
+              dir = "asc";
+              /* Make a loop that will continue until
+              no switching has been done: */
+              while (switching) {
+                // Start by saying: no switching is done:
+                switching = false;
+                rows = table.rows;
+                /* Loop through all table rows (except the
+                first, which contains table headers): */
+                for (i = 1; i < (rows.length - 1); i++) {
+                  // Start by saying there should be no switching:
+                  shouldSwitch = false;
+                  /* Get the two elements you want to compare,
+                  one from current row and one from the next: */
+                  x = rows[i].getElementsByTagName("TD")[n];
+                  y = rows[i + 1].getElementsByTagName("TD")[n];
+                  /* Check if the two rows should switch place,
+                  based on the direction, asc or desc: */
+                  if (dir == "asc") {
+                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                      // If so, mark as a switch and break the loop:
+                      shouldSwitch = true;
+                      break;
+                    }
+                  } else if (dir == "desc") {
+                    if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                      // If so, mark as a switch and break the loop:
+                      shouldSwitch = true;
+                      break;
+                    }
+                  }
+                }
+                if (shouldSwitch) {
+                  /* If a switch has been marked, make the switch
+                  and mark that a switch has been done: */
+                  rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                  switching = true;
+                  // Each time a switch is done, increase this count by 1:
+                  switchcount ++;
+                } else {
+                  /* If no switching has been done AND the direction is "asc",
+                  set the direction to "desc" and run the while loop again. */
+                  if (switchcount == 0 && dir == "asc") {
+                    dir = "desc";
+                    switching = true;
+                  }
+                }
+              }
+            }
+        </script>
     </head>
     <body>
         <table border="1">
             <tr>
-                <th>app id</th>
-                <th>registry date</th>
-                <th>duration (minutes)</th>
-                <th>resident data (gb)</th>
-                <th>partition number/huge partition</th>
-                <th>reported block id number</th>
+                <th onclick="sortTable(0)">app id</th>
+                <th onclick="sortTable(1)">registry date</th>
+                <th onclick="sortTable(2)">duration (minutes)</th>
+                <th onclick="sortTable(3)">resident data (gb)</th>
+                <th onclick="sortTable(4)">partition number/huge partition</th>
+                <th onclick="sortTable(5)">reported block id number</th>
             </tr>
     "#
     .to_string();
