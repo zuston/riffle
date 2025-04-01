cd2dc7a2da4d818a6c61cda61bff28fc5c9efdf2
Junfan Zhang <zuston@apache.org>
2025-04-01 10:32:56 +0800

    feat: Bound server_id into log parent folder

--- src/logforth_service.rs
+++ src/logforth_service.rs
@@ -1,3 +1,4 @@
+use crate::app::SHUFFLE_SERVER_ID;
 use crate::config::LogConfig;
 use logforth::append::rolling_file::{RollingFileWriter, Rotation};
 use logforth::append::{rolling_file, RollingFile};
@@ -10,12 +11,16 @@ impl LogService {
     pub fn init(log: &LogConfig) -> WorkerGuard {
         // todo: obey the rule from the config options
 
+        let default_id = "unknown".to_string();
+        let shuffle_server_id = SHUFFLE_SERVER_ID.get().unwrap_or(&default_id);
+        let child_folder = format!("{}/{}", &log.path, shuffle_server_id);
+
         let rolling_writer = RollingFileWriter::builder()
             .rotation(Rotation::Daily)
             .filename_prefix(LOG_FILE_NAME)
             .max_file_size(512 * 1024 * 1024)
             .max_log_files(10)
-            .build(&log.path)
+            .build(&child_folder)
             .unwrap();
 
         let (non_blocking, _guard) = rolling_file::non_blocking(rolling_writer).finish();
