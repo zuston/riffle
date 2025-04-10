zuston/R1

feat: Add discovery mechanism

src/lib.rs
@@ -55,6 +55,7 @@ pub mod lazy_initializer;
 
 mod config_reconfigure;
 pub mod deadlock;
+pub mod discovery;
 pub mod disk_explorer;
 pub mod historical_apps;
 pub mod panic_hook;

src/main.rs
@@ -64,6 +64,7 @@ pub mod kerberos;
 
 pub mod id_layout;
 
+pub mod discovery;
 pub mod lazy_initializer;
 
 #[cfg(not(feature = "logforth"))]

zuston/riffle

src/lib.rs
@@ -60,6 +60,8 @@ pub mod disk_explorer;
 pub mod historical_apps;
 pub mod panic_hook;
 
+pub mod admin;
+
 use crate::app::{AppManager, AppManagerRef};
 use crate::common::init_global_variable;
 use crate::config_reconfigure::ReconfigurableConfManager;

src/main.rs
@@ -67,6 +67,7 @@ pub mod id_layout;
 
 pub mod decommission;
 pub mod lazy_initializer;
+pub mod admin;
 #[cfg(not(feature = "logforth"))]
 mod log_service;
 