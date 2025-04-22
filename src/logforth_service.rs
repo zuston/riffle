26f20b0cfa148dc808a392225ccc62ccf07f0072
Junfan Zhang <zuston@apache.org>
2025-04-22 17:13:53 +0800

    fix: Remove child log path with server_id

--- src/logforth_service.rs
+++ src/logforth_service.rs
@@ -10,17 +10,12 @@ pub struct LogService;
 impl LogService {
     pub fn init(log: &LogConfig) -> WorkerGuard {
         // todo: obey the rule from the config options
-
-        let default_id = "unknown".to_string();
-        let shuffle_server_id = SHUFFLE_SERVER_ID.get().unwrap_or(&default_id);
-        let child_folder = format!("{}/{}", &log.path, shuffle_server_id);
-
         let rolling_writer = RollingFileWriter::builder()
             .rotation(Rotation::Daily)
             .filename_prefix(LOG_FILE_NAME)
             .max_file_size(512 * 1024 * 1024)
             .max_log_files(10)
-            .build(&child_folder)
+            .build(&log.path)
             .unwrap();
 
         let (non_blocking, _guard) = rolling_file::non_blocking(rolling_writer).finish();
