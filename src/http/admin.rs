c66af530e4f96cce1e00f7d621c7f4047cdeca73
Junfan Zhang <zuston@apache.org>
2025-04-28 15:13:55 +0800

    amend

--- src/http/admin.rs
+++ src/http/admin.rs
@@ -10,10 +10,10 @@ pub struct AdminHandler;
 
 impl Handler for AdminHandler {
     fn get_route_method(&self) -> RouteMethod {
-        RouteMethod::new().get(adminHandler)
+        RouteMethod::new().get(admin_handler)
     }
 
-    /// request with /admin?updateServerState=DECOMMISSION
+    /// request with /admin?update_state=DECOMMISSION
     fn get_route_path(&self) -> String {
         "/admin".to_string()
     }
@@ -21,18 +21,18 @@ impl Handler for AdminHandler {
 
 #[derive(Deserialize)]
 struct OperationParam {
-    update_server_state: Option<ServerState>,
+    update_state: Option<ServerState>,
 }
 
 #[handler]
-fn adminHandler(req: &Request) -> poem::Result<String> {
+fn admin_handler(req: &Request) -> poem::Result<String> {
     let params = req.params::<OperationParam>()?;
     let server_state_manager_ref = SERVER_STATE_MANAGER_REF.get();
     if server_state_manager_ref.is_none() {
         return Ok("Uninitialized server_state_manager. Ingore".to_string());
     }
     let server_state_manager_ref = server_state_manager_ref.unwrap();
-    if let Some(state) = params.update_server_state {
+    if let Some(state) = params.update_state {
         server_state_manager_ref.as_state(state);
     }
 
@@ -51,7 +51,7 @@ mod tests {
         let handler = AdminHandler::default();
         let app = Route::new().at(handler.get_route_path(), handler.get_route_method());
         let cli = TestClient::new(app);
-        let resp = cli.get("/admin?update_server_state=HEALTHY").send().await;
+        let resp = cli.get("/admin?update_state=HEALTHY").send().await;
         resp.assert_status_is_ok();
     }
 }
