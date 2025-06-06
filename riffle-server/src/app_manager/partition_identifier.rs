5be3e3a28f1eede2250ee2c5d17c60be62898bb4
Junfan Zhang <zuston@apache.org>
2025-06-06 16:42:24 +0800

    change the equals sequence of partitionUid for better performance

--- riffle-server/src/app_manager/partition_identifier.rs
+++ riffle-server/src/app_manager/partition_identifier.rs
@@ -1,11 +1,21 @@
 use std::hash::{DefaultHasher, Hash, Hasher};
 
-#[derive(Ord, PartialOrd, Eq, PartialEq, Default, Debug, Hash, Clone)]
+#[derive(Ord, PartialOrd, Default, Debug, Hash, Clone)]
 pub struct PartitionedUId {
     pub app_id: String,
     pub shuffle_id: i32,
     pub partition_id: i32,
 }
+
+impl PartialEq for PartitionedUId {
+    fn eq(&self, other: &Self) -> bool {
+        self.partition_id == other.partition_id
+            && self.shuffle_id == other.shuffle_id
+            && self.app_id == other.app_id
+    }
+}
+
+impl Eq for PartitionedUId {}
 
 impl PartitionedUId {
     pub fn from(app_id: String, shuffle_id: i32, partition_id: i32) -> PartitionedUId {
