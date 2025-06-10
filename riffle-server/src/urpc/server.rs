996326340296f2c24e0813be54b1ae9a65516ed7
Junfan Zhang <zuston@apache.org>
2025-06-10 15:31:32 +0800

    feat(urpc): Add the QPS metric

--- riffle-server/src/urpc/server.rs
+++ riffle-server/src/urpc/server.rs
@@ -12,7 +12,9 @@ use crate::urpc::shutdown::Shutdown;
 use crate::app_manager::AppManagerRef;
 use crate::await_tree::AWAIT_TREE_REGISTRY;
 use crate::error::WorkerError;
-use crate::metric::{URPC_CONNECTION_NUMBER, URPC_REQUEST_PROCESSING_LATENCY};
+use crate::metric::{
+    TOTAL_GRPC_REQUEST, TOTAL_URPC_REQUEST, URPC_CONNECTION_NUMBER, URPC_REQUEST_PROCESSING_LATENCY,
+};
 use crate::urpc::command::Command;
 use anyhow::Result;
 use await_tree::InstrumentAwait;
@@ -109,8 +111,12 @@ impl Handler {
                 None => return Ok(()),
             };
 
+            let path = frame.to_string();
+            TOTAL_URPC_REQUEST.with_label_values(&[&"ALL"]).inc();
+            TOTAL_URPC_REQUEST.with_label_values(&[&path]).inc();
+
             let _ = URPC_REQUEST_PROCESSING_LATENCY
-                .with_label_values(&[&format!("{}", &frame)])
+                .with_label_values(&[&path])
                 .start_timer();
             Command::from_frame(frame)?
                 .apply(
