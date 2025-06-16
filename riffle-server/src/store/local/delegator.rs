3d092d9dfbc2623ebbde412e13a8eb45b32f0fa6
Junfan Zhang <zuston@apache.org>
2025-06-16 11:13:12 +0800

    chore: Disable disk detection on test env (#351)
    * chore: Disable disk detection on test env
    
    * Disable capacity check test

--- riffle-server/src/store/local/delegator.rs
+++ riffle-server/src/store/local/delegator.rs
@@ -123,18 +123,22 @@ impl LocalDiskDelegator {
             }),
         };
 
-        let runtime = runtime_manager.clone().default_runtime.clone();
-        let io_delegator = delegator.clone();
-        let span = format!("disk[{}] checker", root);
-        runtime.spawn_with_await_tree(&span, async move {
-            info!("starting the disk[{}] checker", &io_delegator.inner.root);
-            if let Err(e) = io_delegator.schedule_check().await {
-                error!(
-                    "disk[{}] checker exit. err: {:?}",
-                    &io_delegator.inner.root, e
-                )
-            }
-        });
+        // in test env, this disk detection will always make disk unhealthy status
+        #[cfg(not(test))]
+        {
+            let runtime = runtime_manager.clone().default_runtime.clone();
+            let io_delegator = delegator.clone();
+            let span = format!("disk[{}] checker", root);
+            runtime.spawn_with_await_tree(&span, async move {
+                info!("starting the disk[{}] checker", &io_delegator.inner.root);
+                if let Err(e) = io_delegator.schedule_check().await {
+                    error!(
+                        "disk[{}] checker exit. err: {:?}",
+                        &io_delegator.inner.root, e
+                    )
+                }
+            });
+        }
 
         delegator
     }
@@ -348,6 +352,7 @@ mod test {
     use std::time::Duration;
 
     #[test]
+    #[ignore]
     fn test_capacity_check() -> anyhow::Result<()> {
         let temp_dir = tempdir::TempDir::new("test_sync_io").unwrap();
         let temp_path = temp_dir.path().to_str().unwrap().to_string();
