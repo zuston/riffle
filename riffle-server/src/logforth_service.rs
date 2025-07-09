8db9ff29f4f75291acff364e9eb5e7492e0fb546
Junfan Zhang <zuston@apache.org>
2025-07-09 15:57:12 +0800

    api compatibility

--- riffle-server/src/logforth_service.rs
+++ riffle-server/src/logforth_service.rs
@@ -1,30 +1,24 @@
 use crate::app_manager::SHUFFLE_SERVER_ID;
 use crate::config::LogConfig;
-use logforth::append::rolling_file::{RollingFileWriter, Rotation};
+use logforth::append::rolling_file::{RollingFileBuilder, Rotation};
 use logforth::append::{rolling_file, RollingFile};
-use logforth::non_blocking::WorkerGuard;
+use logforth::DropGuard;
 
 const LOG_FILE_NAME: &str = "riffle-server";
 
 pub struct LogService;
 impl LogService {
-    pub fn init(log: &LogConfig) -> WorkerGuard {
-        // todo: obey the rule from the config options
-        let rolling_writer = RollingFileWriter::builder()
+    pub fn init(log: &LogConfig) -> DropGuard {
+        let (rolling_writer, _guard) = RollingFileBuilder::new("logs")
             .rotation(Rotation::Daily)
             .filename_prefix(LOG_FILE_NAME)
             .max_file_size(512 * 1024 * 1024)
             .max_log_files(10)
-            .build(&log.path)
+            .build()
             .unwrap();
 
-        let (non_blocking, _guard) = rolling_file::non_blocking(rolling_writer).finish();
-
         logforth::builder()
-            .dispatch(|d| {
-                d.filter(log::LevelFilter::Info)
-                    .append(RollingFile::new(non_blocking))
-            })
+            .dispatch(|d| d.filter(log::LevelFilter::Info).append(rolling_writer))
             .apply();
 
         _guard
