05433843942045c1614d0a9103498e89ef9ec3c5
Junfan Zhang <zuston@apache.org>
2025-12-09 19:16:37 +0800

    fix

--- riffle-server/src/rpc.rs
+++ riffle-server/src/rpc.rs
@@ -230,7 +230,11 @@ impl DefaultRpcService {
 }
 
 #[cfg(feature = "urpc_uring")]
-async fn urpc_serve_with_monoio(addr: SocketAddr, shutdown: impl Future, app_manager_ref: AppManagerRef) {
+async fn urpc_serve_with_monoio(
+    addr: SocketAddr,
+    shutdown: impl Future,
+    app_manager_ref: AppManagerRef,
+) {
     let listner: monoio::net::TcpListener = monoio::net::TcpListener::bind(addr).unwrap();
     let _ = urpc_uring::server::run(listner, shutdown, app_manager_ref).await;
 }
