91027f3126466671faae88b135a4c4e99ef76208
Junfan Zhang <zuston@apache.org>
2025-12-26 17:44:21 +0800

    feat(hard-split): Standardize naming by hard-split and add metric (#565)

--- riffle-server/src/error.rs
+++ riffle-server/src/error.rs
@@ -55,8 +55,8 @@ pub enum WorkerError {
     #[error("The memory usage is limited by huge partition mechanism")]
     MEMORY_USAGE_LIMITED_BY_HUGE_PARTITION,
 
-    #[error("shuffle writing is limited due to the unhealthy storage state")]
-    WRITE_LIMITED_BY_STORAGE_STATE,
+    #[error("trigger hard split due to the unhealthy storage state")]
+    HARD_SPLIT_BY_UNHEALTHY_STORAGE,
 
     #[error("Http request failed. {0}")]
     HTTP_SERVICE_ERROR(String),
